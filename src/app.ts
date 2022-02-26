9cd153138f55d56036bfa221fbc735878e805278
bright.ikhide <bright.ikhide@e4email.net>
2022-02-26 17:34:23 +0100

    changed order of results

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,21 @@ import sequelize from "./database";
 connectModels(sequelize);
 const app = express();
 
-app.use(cors());
+const whitelist = [
+  "http://localhost:8080",
+  "https://narrativemarket.netlify.app",
+];
+const corsOptions = {
+  origin: function (origin: string, callback: any) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Blocked!"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use(appRouter);
